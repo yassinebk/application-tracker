06454ce98fef923ad38fa6d3e400d031d05d6a2a
yassinebk <yassinebk23@gmail.com>
2024-09-17 22:43:34 +0100

    feat: add a favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
