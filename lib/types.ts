16c4ae4455870c39df35b9bb35bec2ae17b8a564
yassinebk <yassinebk23@gmail.com>
2024-09-21 01:46:15 +0100

    fix: prelaod tw classes

--- lib/types.ts
+++ lib/types.ts
@@ -14,13 +14,11 @@ export interface Application {
 export const mapStatusToBadge = (status: string) => {
   switch (status) {
     case "TO_APPLY":
-      return "bg-gray-200 text-gray-800";
+      return "bg-gray-200 text-gray-900";
     case "PENDING":
-      return "bg-yellow-200 text-yellow-800";
+      return "bg-yellow-200 text-gray-900";
     case "CANCELED":
-      return "bg-red-200 text-red-800";
-    case "REFUSED":
-      return "bg-red-500 text-white";
+      return "bg-red-900 text-red-800";
     case "IN_INTERVIEWS":
       return "bg-blue-200 text-blue-800";
     default:
